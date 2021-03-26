3c05a7a4c3023331f82cdf466f42f07167198270
XieJiongyan <joy2home@outlook.com>
2021-03-26 22:03:37 +0800

    J.0.2

--- test.cpp
+++ test.cpp
@@ -8,22 +8,6 @@
 using namespace::std;
 using namespace::J;
 int main() {
-    // set<int> s;
-    // s.insert(0);
-    // s.insert(1);
-    // s.insert(2);
-    // s.insert(3);
-    // s.insert(4);
-    // cout << *s.begin() << endl;
-    map<int, int> m;
-    pair<string, int> p;
-    m[0] = 0;
-    m.begin()->second = 1;
-    cout << m.begin()->second << endl;
-    // m[1] = 1;
-    // m[2] = 2;
-    // // cout << accumulate(m.begin(), m.end(), 0) << endl;
-    // cout << *(--s.end()) << endl;
     cout << "Start test range vessel" << endl;
     _Section_node_base section_node_base;
     cout << "J.1.0 Define object _Section_node_base, with default constructor, get its parent: " << section_node_base._parent << endl;
